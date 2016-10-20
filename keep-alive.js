a1ef1217982d8ea4af89b5065359ceb5561ea790
Ankur Goel <ankurgel@gmail.com>
2016-10-21 02:56:26 +0530

    Hook keepAlive in globalAgent directly - https://nodejs.org/api/http.html#http_http_globalagent

--- keep-alive.js
+++ keep-alive.js
@@ -1,26 +1,6 @@
 var http = require('http'),
     https = require('https');
 
-var agent = new http.Agent({
-  keepAlive: true,
-  keepAliveMsecs: 30*1000
-});
+http.globalAgent.keepAlive = true;
+http.globalAgent.keepAliveMsecs = 30*1000;
 
-var secureAgent = new https.Agent({
-  keepAlive: true,
-  keepAliveMsecs: 30*1000
-});
-
-var httpRequest = http.request;
-var httpsRequest = http.request;
-
-http.request = function(options, callback){
-  if(options.protocol == "https:"){
-    options["agent"] = secureAgent;
-    return httpsRequest(options, callback);  
-  }
-  else {
-    options["agent"] = agent;
-    return httpRequest(options, callback);
-  }
-};
